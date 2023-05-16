ccd8e5feb79fe60a7037ac01cd2320b7bad65360
abanishtr <abanisht558@gmail.com>
2023-05-16 16:58:35 +0545

    solved bugs

--- src/components/About.js
+++ src/components/About.js
@@ -2,102 +2,104 @@ import React from 'react'
 
 export default function About() {
     return (
-      <div ClassName='container'>
-        <div Class="accordion accordion-flush" id="accordionFlushExample">
-  <div Class="accordion-item">
-    <h2 Class="accordion-header" id="flush-headingOne">
-      <button
-        Class="accordion-button collapsed"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#flush-collapseOne"
-        aria-expanded="false"
-        aria-controls="flush-collapseOne"
-      >
-        Accordion Item #1
-      </button>
-    </h2>
-    <div
-      id="flush-collapseOne"
-      Class="accordion-collapse collapse"
-      aria-labelledby="flush-headingOne"
-      data-mdb-parent="#accordionFlushExample"
-    >
-      <div Class="accordion-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch
-        et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-        sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-        craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
-        heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div Class="accordion-item">
-    <h2 Class="accordion-header" id="flush-headingTwo">
-      <button
-        Class="accordion-button collapsed"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#flush-collapseTwo"
-        aria-expanded="false"
-        aria-controls="flush-collapseTwo"
-      >
-        Accordion Item #2
-      </button>
-    </h2>
-    <div
-      id="flush-collapseTwo"
-      Class="accordion-collapse collapse"
-      aria-labelledby="flush-headingTwo"
-      data-mdb-parent="#accordionFlushExample"
-    >
-      <div Class="accordion-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch
-        et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-        sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-        craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
-        heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div Class="accordion-item">
-    <h2 Class="accordion-header" id="flush-headingThree">
-      <button
-        Class="accordion-button collapsed"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#flush-collapseThree"
-        aria-expanded="false"
-        aria-controls="flush-collapseThree"
-      >
-        Accordion Item #3
-      </button>
-    </h2>
-    <div
-      id="flush-collapseThree"
-      Class="accordion-collapse collapse"
-      aria-labelledby="flush-headingThree"
-      data-mdb-parent="#accordionFlushExample"
-    >
-      <div Class="accordion-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch
-        et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-        sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-        craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
-        heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
-      </div>
+        <div className='container'>
+            <h1 className='my-3 mx-3'>About Us</h1>
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingOne">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-mdb-toggle="collapse"
+                            data-mdb-target="#flush-collapseOne"
+                            aria-expanded="false"
+                            aria-controls="flush-collapseOne"
+                        >
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div
+                        id="flush-collapseOne"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingOne"
+                        data-mdb-parent="#accordionFlushExample"
+                    >
+                        <div className="accordion-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                            richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+                            brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                            sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch
+                            et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                            sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
+                            craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
+                            heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingTwo">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-mdb-toggle="collapse"
+                            data-mdb-target="#flush-collapseTwo"
+                            aria-expanded="false"
+                            aria-controls="flush-collapseTwo"
+                        >
+                            Accordion Item #2
+                        </button>
+                    </h2>
+                    <div
+                        id="flush-collapseTwo"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingTwo"
+                        data-mdb-parent="#accordionFlushExample"
+                    >
+                        <div className="accordion-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                            richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+                            brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                            sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch
+                            et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                            sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
+                            craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
+                            heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingThree">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-mdb-toggle="collapse"
+                            data-mdb-target="#flush-collapseThree"
+                            aria-expanded="false"
+                            aria-controls="flush-collapseThree"
+                        >
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div
+                        id="flush-collapseThree"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingThree"
+                        data-mdb-parent="#accordionFlushExample"
+                    >
+                        <div className="accordion-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                            richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+                            brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                            sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch
+                            et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                            sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
+                            craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
+                            heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button className="btn btn-dark mx-3 mt-3">Enable Dark mode</button>
+        </div>
     )
 }
