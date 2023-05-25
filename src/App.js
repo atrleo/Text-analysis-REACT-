0a29600f19a06c4ffe1e1161b15e83846fa02b6b
abanishtr <abanisht558@gmail.com>
2023-05-25 18:56:16 +0545

    Created an alert message for DarkMode and lightMode

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
   const [mode, setMode] = useState('light');
   const [change, setChange] = useState("Enable DarkMode ")
-  const toggleMode = () => {
-    if(mode === 'light'){
-      setMode('dark');
-      setChange('Enable LightMode');
-      document.body.style.backgroundColor = "#4f4f4f";
-    }
-    else{
-      setMode('light');
-      setChange('Enable DarkMode');
-      document.body.style.backgroundColor = "white";
-    }
+  const [alert, setAlert] = useState(null);
+  const showAlert = (messege, type) => {
+    setAlert({
+     msg:messege,
+      type: type
+    });
   }
-  return (
-    <>
-      {/* <Navbar title="textUtils2" home="home2" /> */}
-      <Navbar title="TextUtelsis" mode={mode} toggleMode={toggleMode} change={change}/>
-
-      <div className="container my-3" >
-    <TextForm head="Enter your text to ANALYZE" mode={mode} summary="Your text summary " prev="Preview" /> 
-        {/* <About /> */}
-
-      </div>
 
 
 
-    </>
-  );
+
+const toggleMode = () => {
+  if (mode === 'light') {
+    setMode('dark');
+    setChange('Enable LightMode');
+    document.body.style.backgroundColor = "#4f4f4f";
+    showAlert('DarkMode has been Enabled','success');
+  }
+  else {
+    setMode('light');
+    setChange('Enable DarkMode');
+    document.body.style.backgroundColor = "white";
+    showAlert('Light mode has been Enabled','success');
+  }
+};
+
+return (
+  <>
+    {/* <Navbar title="textUtils2" home="home2" /> */}
+    <Navbar title="TextUtelsis" mode={mode} toggleMode={toggleMode} change={change} />
+    <Alert alert={alert} />
+
+    <div className="container my-3" >
+      <TextForm head="Enter your text to ANALYZE" mode={mode} summary="Your text summary " prev="Preview" />
+      {/* <About /> */}
+
+    </div>
+
+
+
+  </>
+);
 }
+
 
 export default App;
 
